4b4ae3b5377403abe3161fbe111f85c21de1cc52
Jacob <osarukun@live.com>
2025-11-11 23:56:25 -0500

    wip: basic react-router placements for mainpage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,36 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { BrowserRouter, Routes, Route, NavLink } from "react-router";
 
-function App() {
-  const [count, setCount] = useState(0);
+import Dashboard from "./pages/dashboard";
+import "./stylesheets/main.scss";
 
+const App: React.FC = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <nav className="navbar">
+        <div className="left-tabs">
+          <NavLink
+            to="/"
+            end
+            className={({ isActive }) => (isActive ? "tab active" : "tab")}
+          >
+            Dashboard
+          </NavLink>
+          <NavLink
+            to="/income"
+            end
+            className={({ isActive }) => (isActive ? "tab active" : "tab")}
+          >
+            Income
+          </NavLink>
+        </div>
+      </nav>
+      <main>
+        <Routes>
+          <Route path="/" element={<Dashboard />}></Route>
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
